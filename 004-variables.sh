aa3357443dfae3601ba2822445c1cef218169bb9
Mukta06 <cbmukta06@gmail.com>
2024-03-19 17:07:35 +0530

    Bash Auto Push

---
+++ 004-variables.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Declaring variable
+
+a=10
+b=def
+
+
+# echo a --> will not print the value of the variable a,,,, to print it use $
+# $--> is the special variable which will print the value of the variable
+
+#Printing the value of the variable
+
+echo $a
+echo "The value of the variable b is $b"
