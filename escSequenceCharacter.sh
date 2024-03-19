2db48a6c9e4489615f0178b3faa315573ad30515
Mukta06 <cbmukta06@gmail.com>
2024-03-19 15:59:53 +0530

    Bash Auto Push

---
+++ escSequenceCharacter.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Escape sequence characters
+#\n : New line
+#\t : Tab space
+
+echo "Use \n to print in new line"
+echo "Use \t to give tab space"
+
+echo "Line3\nLine4---> In this \n will not be considered as escape character"
+echo "To enable escape characters use echo -e then printing statement"
+
