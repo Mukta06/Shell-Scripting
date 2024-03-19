09887bcadc6d0f5cd2e17e117b0fd03d992654ef
Mukta06 <cbmukta06@gmail.com>
2024-03-19 17:20:01 +0530

    Bash Auto Push

--- 004-variables.sh
+++ 004-variables.sh
@@ -15,3 +15,12 @@ echo $a
 # echo $a OR echo ${a}--> both are same if in some versions of bash 
 
 echo "The value of the variable b is $b"
+
+# If we try to print the value of the variable which is not declared it will print null
+# Same in case deleting dir or file which is not present will delect the entire parent dir 
+# rm -rf /data/prod/${APPDATA}---> incase APPDATA is not present then entire prod dir will be deleted.
+
+
+# Print the value in different colors 
+
+echo -e " Printing the values in color \e[31m ${b} \e[0m"
