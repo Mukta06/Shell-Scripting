5be3be7cdcb0a9b0da99f000b398581a932f4f53
Mukta06 <cbmukta06@gmail.com>
2024-03-19 16:03:01 +0530

    Bash Auto Push

--- escSequenceCharacter.sh
+++ escSequenceCharacter.sh
@@ -10,5 +10,5 @@ echo "Use \t to give tab space"
 echo "Line3\nLine4---> In this \n will not be considered as escape character"
 echo "To enable escape characters use echo -e then printing statement"
 
-echo -e "Line3\nLine4"
+echo -e "Line3\nLine4\tUsed new line and Tab"
 
