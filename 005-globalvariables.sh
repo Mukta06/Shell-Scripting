de153bdb7f2a93bf422d8a2ac4fabba2d9b640ea
Mukta06 <cbmukta06@gmail.com>
2024-03-19 18:03:24 +0530

    Bash Auto Push

---
+++ 005-globalvariables.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# display time date and no of sessions opended by hardcoding the value 
+
+DATE="19/03/2024"
+No_of_sessions=4
+
+echo "Todays date is $DATE"
+echo "No of sessions opened is ${No_of_sessions}"
+
+#How to display time date and no of sessions opended without hardcoding the value 
+
+Date="$(date +%F)"
+Sessions= "$(who|wc -l)"
+
+echo "Todays date using dynamic date is : ${Date}"
+echo "No of opened sessions by dynamic computing is : ${Sessions}"
