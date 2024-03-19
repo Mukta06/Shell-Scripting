0bf662fe2f75ddff5319d6f476de5aec0910c6ea
Mukta06 <cbmukta06@gmail.com>
2024-03-19 15:41:17 +0530

    Bash Auto Push

---
+++ 001-sheband.sh
@@ -0,0 +1,7 @@
+#!/bin/shell
+
+#Sheband notation : it tells the script, with which interpretor the script should be executed.
+
+#There can be only one Interpretor per script and that should be always on the 1st line of the script.
+
+echo Welcome To Bash Scripting.
