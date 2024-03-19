fc38cc49b96349f5c6ca1ec7934fbeb398d93b84
Mukta06 <cbmukta06@gmail.com>
2024-03-19 16:55:05 +0530

    Bash Auto Push

--- 003-colors.sh
+++ 003-colors.sh
@@ -2,6 +2,10 @@
 
 #Syntax to use color 
 
-# echo -e "\e[COLORCODEm I am Priniting Color \e[0m"
+# echo -e "\e[COLORCODEm I am Priniting Color \e[0m"  ----> Foreground color syntax---> text color will be changed
 
 echo -e "\e[32mI am printing GREEN COLOR\e[0m"
+
+# echo -e "\e[BG color code;text colorcode m I am printing background and foreground color \e[0m"
+
+echo -e "\e[43;31m I am printing both background and foreground color \e[0m"
