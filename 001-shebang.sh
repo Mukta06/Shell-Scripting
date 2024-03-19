b96845782acd2d597265e122f41632a6f9fca89f
Mukta06 <cbmukta06@gmail.com>
2024-03-19 15:52:25 +0530

    Bash Auto Push

--- 001-shebang.sh
+++ 001-shebang.sh
@@ -1,6 +1,6 @@
 #!/bin/shell
 
-#Sheband notation : it tells the script, with which interpretor the script should be executed.
+#Shebang notation : it tells the script, with which interpretor the script should be executed.
 
 #There can be only one Interpretor per script and that should be always on the 1st line of the script.
 
