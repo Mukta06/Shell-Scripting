dfeb2419441be0b3ca2f3befdd834457f30033d1
Mukta06 <cbmukta06@gmail.com>
2024-03-20 17:29:55 +0530

    Bash Auto Push

---
+++ 008-read.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# To read the input from the treminal which will break the execution of the script 
+
+read -p "Enter the name :" name 
+echo  "The name of the person executing the script is : $name"
