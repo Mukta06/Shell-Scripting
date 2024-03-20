4273d86c57f7348291af6884fe0f13845ccfbfb0
Mukta06 <cbmukta06@gmail.com>
2024-03-20 12:25:56 +0530

    Bash Auto Push

---
+++ 006-specialvariables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Special variabes are pre defined variables with specific pre defined powers.
+# We cannot create special pre defined variables we can just use it 
+
+a=3000
+
+echo "The value of a is : $a"
+
+echo "The value of special variable 0 is : $0"
