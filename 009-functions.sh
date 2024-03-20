0a677feebfe6ab1d9db1138c2b875db4b7f6dbd3
Mukta06 <cbmukta06@gmail.com>
2024-03-20 23:46:54 +0530

    Bash Auto Push

--- 009-functions.sh
+++ 009-functions.sh
@@ -19,7 +19,7 @@ start()
     f
     echo "Number of opened sessions : $(who|wc -l)"
     echo "Today's date is : $(date + %F)"
-    echo "Load average in last 1 min is : $(uptime|awk -F :'{print $NF}'|awk -F ',' '{print $1}')"
+    echo "Load average in last 1 min is : $( uptime | awk -F : '{print $NF}' | awk -F ',' '{print $1}')"
 }
 
 start
