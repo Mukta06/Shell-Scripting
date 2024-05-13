ce6cc51e58f4f2f57efaffbdceea998fae6dc83c
Mukta06 <cbmukta06@gmail.com>
2024-05-13 17:18:57 +0530

    Bash Auto Push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -4,15 +4,17 @@ echo  -e "\e[32m I am cart frontend \e[0m"
 
 # Check whether the user have root access,, if not exit the script
 
-ID= $(id -u)
-LOGFILE= "/tmp/frontend.log"
+ID = $(id -u)
+
 
 if [ $ID -ne 0 ]; then
     echo -e "\e[31m This script is expected to run with sudo \e[0m \n\e[33m EX : sudo bash Scriptname\e[0m"
     exit 1
 fi
 
-echo "Installing Nginx web server" 
+LOGFILE = "/tmp/frontend.log"
+
+echo -n "Installing Nginx web server :" 
 dnf install nginx -y      &>>  $LOGFILE
 if [ $? -eq 0 ]; then
     echo -e "\e[32m SUCCESS \e[0m"
@@ -20,7 +22,7 @@ else
     echo -e "\e[31m FAILURE \e[0m"
 fi
 
-echo "Enabling the Nginx server"
+echo  -n "Enabling the Nginx server :"
 systemctl enable nginx    &>>  $LOGFILE
 if [ $? -eq 0 ]; then
     echo -e "\e[32m SUCCESS \e[0m"
@@ -28,7 +30,7 @@ else
     echo -e "\e[31m FAILURE \e[0m"
 fi
 
-echo "starting the Nginx server"
+echo -n "starting the Nginx server :"
 systemctl start nginx     &>>  $LOGFILE
 if [ $? -eq 0 ]; then
     echo -e "\e[32m SUCCESS \e[0m"
