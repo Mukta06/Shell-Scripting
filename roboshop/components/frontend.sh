ef5cf2c39900b64ded6d0eb552b43145408734e1
Mukta06 <cbmukta06@gmail.com>
2024-05-13 16:57:23 +0530

    Bash Auto Push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -5,16 +5,16 @@ echo  -e "\e[32m I am cart frontend \e[0m"
 # Check whether the user have root access,, if not exit the script
 
 ID=$(id -u)
+
 if [ $ID -ne 0 ]; then
     echo -e "\e[31m This script is expected to run with sudo \e[0m \n\e[33m EX : sudo bash Scriptname\e[0m"
     exit 1
 fi
 
 echo "Installing Nginx web server" 
-dnf install nginx -y      &>>  tmp/logs/frontend.log
+dnf install nginx -y      &>>  frontend.log
 
 echo "Enabling the Nginx server"
-systemctl enable nginx    &>>  tmp/logs/frontend.log
-
+systemctl enable nginx    &>>  frontend.log
 echo "starting the Nginx server"
-systemctl start nginx     &>>  tmp/logs/frontend.log
+systemctl start nginx     &>>  frontend.log
