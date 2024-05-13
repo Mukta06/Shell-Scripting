9d9a1a33cd8491b33ed7aa544855158f55d55f51
Mukta06 <cbmukta06@gmail.com>
2024-05-13 15:46:33 +0530

    Bash Auto Push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,3 +1,10 @@
 #!bin/bash
 
 echo  -e "\e[32m I am cart frontend \e[0m"
+
+echo "Installing Nginx web server" 
+dnf install nginx -y 
+echo "Enabling the Nginx server"
+systemctl enable nginx
+echo "starting the Nginx server"
+systemctl start nginx
