d9b83974a03fe5c6a49c5a8be2ccb7a91d414559
Mukta06 <cbmukta06@gmail.com>
2024-05-13 17:31:29 +0530

    Bash Auto Push

--- roboshop/wrapper.sh
+++ roboshop/wrapper.sh
@@ -1,5 +1,5 @@
 #!bin/bash
 
-echo "\e[m34 ***************Running $0 Script*************** \e[0m"
+echo -e  "\e[m34 ***************Running $0 Script*************** \e[0m \n "
 
 bash components/$1.sh
