8600ee566182b376a88d631437a676809760d675
Mukta06 <cbmukta06@gmail.com>
2024-05-13 17:37:30 +0530

    Bash Auto Push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -10,27 +10,23 @@ if [ $ID -ne 0 ]; then
     echo -e "\e[31m This script is expected to run with sudo \e[0m \n\e[33m EX : sudo bash Scriptname\e[0m"
     exit 1
 fi
+status(){
+    if [ $1 -eq 0 ]; then
+        echo -e "\e[32m SUCCESS \e[0m"
+    else
+        echo -e "\e[31m FAILURE \e[0m"
+    fi
+
+}
 
 echo -n "Installing Nginx web server :" 
 dnf install nginx -y      &>>  $LOGFILE
-if [ $? -eq 0 ]; then
-    echo -e "\e[32m SUCCESS \e[0m"
-else
-    echo -e "\e[31m FAILURE \e[0m"
-fi
+status $?
 
 echo  -n "Enabling the Nginx server :"
 systemctl enable nginx    &>>  $LOGFILE
-if [ $? -eq 0 ]; then
-    echo -e "\e[32m SUCCESS \e[0m"
-else
-    echo -e "\e[31m FAILURE \e[0m"
-fi
+status $?
 
 echo -n "starting the Nginx server :"
 systemctl start nginx     &>>  $LOGFILE
-if [ $? -eq 0 ]; then
-    echo -e "\e[32m SUCCESS \e[0m"
-else
-    echo -e "\e[31m FAILURE \e[0m"
-fi
+status $?
