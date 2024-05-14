17ad10872b910b047209760d64244610d15148b8
Mukta06 <cbmukta06@gmail.com>
2024-05-14 14:21:50 +0530

    Bash Auto Push

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -1,9 +1,10 @@
 #!bin/bash 
 
+
+COMPONENT="mongodb"
 echo -e "\e[35m ********_________ $COMPONENT Component Configuration Is Started __________******** \e[0m"
 
 ID=$(id -u)
-COMPONENT="mongodb"
 LOGFILE="/tmp/$COMPONENT.log"
 MONGO_REPO="https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo"
 SCHEMA_URL="https://github.com/stans-robot-project/mongodb/archive/main.zip"
