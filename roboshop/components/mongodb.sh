b3b0f3172eca4ce13c817b9e9befc224fc2e6df0
Mukta06 <cbmukta06@gmail.com>
2024-05-14 13:27:43 +0530

    Bash Auto Push

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -1,3 +1,41 @@
 #!bin/bash 
 
-echo -e "\e[34m I am MongoDB \e[0m"
+echo -e "\e[35m ********_________ Component Configuration Is Started__________******** \e[0m"
+
+ID=$(id -u)
+COMPONENT="mongodb"
+LOGFILE="/tmp/$COMPONENT.log"
+MONGO_REPO="https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo"
+
+if[$ID -ne 0]; then
+    echo "\e[31m This Script is expected to run with SUDO \nEX: sudo bash Scriptname \e[0m"
+    exit 1
+fi
+
+status(){
+    if[$1 -eq 0];then
+        echo "\e32m SUCCESS \e0m"
+    else
+        echo "\e[31m FAILURE \e0m"
+
+    fi
+
+}
+
+echo -n "Configuring $COMPONENT repo :"
+
+curl  $MONGO_REPO > /etc/yum.repos.d/mongodb.repo &>> $LOGFILE
+# curl -s -o /etc/yum.repos.d/mongodb.repo $MONGO_REPO    ----> We can use any of this curl command 
+status $?
+
+echo -n "Installing $COMPONENT : "
+dnf install -y mongodb-org   &>> $LOGFILE
+status $?
+
+echo -n "Enabling $COMPONENT service : " 
+systemctl enable mongod      &>> $LOGFILE
+status $?
+
+echo -n "Starting $COMPONENT : "
+systemctl start mongod      &>> $LOGFILE
+status $?
