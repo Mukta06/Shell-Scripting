1b8864d1b42fc014865163942360035a916ae478
Mukta06 <cbmukta06@gmail.com>
2024-05-14 14:38:54 +0530

    Bash Auto Push

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -26,7 +26,7 @@ status(){
 
 echo -n "Configuring $COMPONENT repo :"
 
-curl  $MONGO_REPO > /etc/yum.repos.d/mongodb.repo &>> $LOGFILE
+curl -s -o /etc/yum.repos.d/mongodb.repo $MONGO_REPO  &>> $LOGFILE
 # curl -s -o /etc/yum.repos.d/mongodb.repo $MONGO_REPO    ----> We can use any of this curl command 
 status $?
 
