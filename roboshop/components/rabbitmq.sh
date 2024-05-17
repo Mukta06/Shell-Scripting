88da3b901bea1503cedb92eba6a1077f974cdfcc
Mukta06 <cbmukta06@gmail.com>
2024-05-17 20:13:07 +0530

    Bash Auto Push

--- roboshop/components/rabbitmq.sh
+++ roboshop/components/rabbitmq.sh
@@ -4,20 +4,32 @@ COMPONENT="rabbitmq"
 echo -e "\n\e[35m ********__________$COMPONENT Component Configuration Is Started ********__________ \e[0m"
 source components/common.sh
 
-# curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | sudo bash
 
-# curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash
+echo -n "Downloading Dependency and Repository required for the $COMPONENT : "
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | sudo bash  &>>LOGFILE
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash  &>>LOGFILE
+status $?
 
-# dnf install rabbitmq-server -y
+echo -n "Installing $COMPONENT Component : "
+dnf install rabbitmq-server -y  &>>LOGFILE
+status $?
 
-# systemctl enable rabbitmq-server 
-# systemctl start rabbitmq-server
-# systemctl status rabbitmq-server -l
+echo -n "Enable and Start $COMPONENT Services : "
+systemctl enable rabbitmq-server  &>>LOGFILE
+systemctl start rabbitmq-server   &>>LOGFILE
+systemctl status rabbitmq-server -l    &>>LOGFILE
 
-Look for the message: Started RabbitMQ broker
+rabbitmqctl list_users | grep $APPUSER    &>>LOGFILE
+if [ $? -ne 0 ];then
+    echo -n "Creating AppUser for $COMPONENT : "
+    rabbitmqctl add_user roboshop roboshop123
+    status $?
+fi
+
+echo -n "SOrting Permission of $COMPONENT : "
+rabbitmqctl set_user_tags roboshop administrator   &>>LOGFILE
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"   &>>LOGFILE
+status $?
 
 
-# rabbitmqctl add_user roboshop roboshop123
-# rabbitmqctl set_user_tags roboshop administrator
-# rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
-
+echo -e "\e[35m ********__________$COMPONENT Configuration Is Completed ___________******** \e[0m"
