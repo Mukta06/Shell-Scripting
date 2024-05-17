6b476d6d3548d540e0b71973119289b4ea10487b
Mukta06 <cbmukta06@gmail.com>
2024-05-17 22:57:12 +0530

    Bash Auto Push

--- roboshop/components/payment.sh
+++ roboshop/components/payment.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 
+$COMPONENT="payment"
+echo -e "\n\e[35m ********__________$COMPONENT Component Configuration Is Started ********__________ \e[0m"
+source components/common.sh
+PYTHON
+echo -e "\n\e[35m ********__________$COMPONENT Component Configuration Is Started ********__________ \e[0m"
