c8afb5cc82357bc52f203f9e5c9e0de9c7c0150a
Mukta06 <cbmukta06@gmail.com>
2024-06-24 23:13:17 +0530

    Bash Auto Push

--- roboshop/components/launch-ec2.sh
+++ roboshop/components/launch-ec2.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # This Script Creates EC2 Instance 
-
+COMPONENT=launchec2
+LOGFILE="/tmp/$COMPONENT.log"
 AMI_ID="ami-072983368f2a6eab5"
 SGID="sg-083812cca5abcd144"
-COMPONENT=$1
+SERVER=$1
 
-aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SGID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]"   
+aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SGID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$SERVER}]"    &>> 
