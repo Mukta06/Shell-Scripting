b671a359a69ef1770541a642498c8a46caee26f9
Mukta06 <cbmukta06@gmail.com>
2024-06-24 23:17:50 +0530

    Bash Auto Push

--- roboshop/components/launch-ec2.sh
+++ roboshop/components/launch-ec2.sh
@@ -7,4 +7,9 @@ AMI_ID="ami-072983368f2a6eab5"
 SGID="sg-083812cca5abcd144"
 SERVER=$1
 
+
+if[ -z $1 ];then
+    echo -e "\e[31m COMPONENT NAME IS NEEDED : \e[0m"
+    exit 1
+fi
 aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SGID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$SERVER}]"    &>>$LOGFILE
