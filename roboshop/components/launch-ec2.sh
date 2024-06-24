0bb89a5b38fb4bdd88cb94930f705b5c0a456919
Mukta06 <cbmukta06@gmail.com>
2024-06-24 23:13:51 +0530

    Bash Auto Push

--- roboshop/components/launch-ec2.sh
+++ roboshop/components/launch-ec2.sh
@@ -7,4 +7,4 @@ AMI_ID="ami-072983368f2a6eab5"
 SGID="sg-083812cca5abcd144"
 SERVER=$1
 
-aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SGID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$SERVER}]"    &>> 
+aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SGID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$SERVER}]"    &>>$LOGFILE
