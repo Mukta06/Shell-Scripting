1e200a63eb361cd095c486b146cc637514e9241c
Mukta06 <cbmukta06@gmail.com>
2024-06-25 18:10:22 +0530

    Bash Auto Push

--- roboshop/components/launch-ec2.sh
+++ roboshop/components/launch-ec2.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# This Script Creates EC2 Instance 
+# This Script Creates EC2 Instance and associated DNS Record as well
 COMPONENT=launchec2
-LOGFILE="/tmp/$COMPONENT.log"
+#LOGFILE="/tmp/$COMPONENT.log"
 AMI_ID="ami-072983368f2a6eab5"
 SGID="sg-083812cca5abcd144"
 SERVER=$1
 
-
+# If the arg is empty ,,, execution exits from if condition
 if [ -z $1 ] ; then
     echo -e "\e[31m COMPONENT NAME IS NEEDED : \e[0m"
     exit 1
 fi
-aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SGID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$SERVER}]"    &>>$LOGFILE
+aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SGID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$SERVER}]"
