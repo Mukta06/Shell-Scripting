d1a1f952cc46c064d86f5e40588c1c6bea735f4f
Mukta06 <cbmukta06@gmail.com>
2024-07-10 11:00:15 +0530

    Ansible Auto Push

--- roboshop/components/launch-ec2.sh
+++ roboshop/components/launch-ec2.sh
@@ -20,7 +20,7 @@ if [ -z $1 ] || [ -z $2 ]; then
 fi
 
 create_ec2() {
-    PRIVATE_IP=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SGID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${SERVER}-${ENV}}]" | jq .Instances[].PrivateIpAddress | sed -e 's/"//g')
+    PRIVATE_IP=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SGID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${SERVER}-${ENV}}]" | jq .Instances[].PrivateIpAddress | sed -e 's/"//g')
 
     echo -e "$COLOR $1 Server is Created and the  IP ADDRESS is : $NOCOLOR $PRIVATE_IP"
 
